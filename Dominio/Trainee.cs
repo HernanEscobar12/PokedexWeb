fce3f67e1da0368d82a5ec82b5cf3eea59a890b3
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:44:11 -0300

    Clase creada para obj Usuarios

---
+++ Dominio/Trainee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value != "")
+                    email = value;
+                else
+                    throw new Exception("email vacío en el dominio...");
+            }
+        }
+
+        public string Pass { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string ImagenPerfil { get; set; }
+        public bool Admin { get; set; }
+
+
+    }
+}
