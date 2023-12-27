a7027101496526e691d9b5dff0f31976675cb77e
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:12:30 -0300

    Vista Default con Repeatear. vista de Pokemon

--- AppWep/Default.aspx.cs
+++ AppWep/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,22 @@ namespace AppWep
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Pokemon> ListaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            PokemonNegocio negocio = new PokemonNegocio();
+            ListaPokemon = negocio.ListarConSP();
 
+            if (!IsPostBack)
+            {
+                repRepetidor.DataSource = ListaPokemon;
+                repRepetidor.DataBind();
+            }
+        }
+
+        protected void btnEjemplo_Click(object sender, EventArgs e)
+        {
+            string valor = ((Button)sender).CommandArgument;
         }
     }
 }
