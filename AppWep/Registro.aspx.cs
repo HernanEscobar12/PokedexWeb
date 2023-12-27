27e759a64a883ea042150ee9bd8d2636a347096d
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:57:33 -0300

    Registro User -  manejo de error - Envio Email Bienvenida

--- AppWep/Registro.aspx.cs
+++ AppWep/Registro.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,31 @@ namespace AppWep
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Trainee user = new Trainee();
+                TraineeNegocio traineeNegocio = new TraineeNegocio();
+                EmailService emailService = new EmailService();
+
+                user.Email = txtEmail.Text;
+                user.Pass = txtPassword.Text;
+                user.Id = traineeNegocio.InsertarNuevo(user);
+                Session.Add("Trainee", user);
+
+                emailService.ArmarCorreo(user.Email, "Bienvenida Trainee", "Hola te damos la bienvenida a la aplicación...");
+                emailService.EnviarEmail();
+                Response.Redirect("Default.aspx", false);
+
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("Error.aspx", false);
+            }
         }
     }
 }
