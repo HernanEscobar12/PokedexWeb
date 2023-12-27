e0506b009d5255a3dd7aa1725ecd2ff10181f5cf
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:24:52 -0300

    Funcionalidad a Vista Formulario

--- AppWep/FormularioPokemon.aspx.cs
+++ AppWep/FormularioPokemon.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,144 @@ namespace AppWep
 {
     public partial class FormularioPokemon : System.Web.UI.Page
     {
+        public bool ConfirmaEliminacion { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            txtId.Enabled = false;
+            ConfirmaEliminacion = false;
+            try
+            {
+                //configuración inicial de la pantalla.
+                if (!IsPostBack)
+                {
+                    ElementoNegocio negocio = new ElementoNegocio();
+                    List<Elemento> lista = negocio.Listar();
 
+                    ddlTipo.DataSource = lista;
+                    ddlTipo.DataValueField = "Id";
+                    ddlTipo.DataTextField = "Descripcion";
+                    ddlTipo.DataBind();
+
+                    ddlDebilidad.DataSource = lista;
+                    ddlDebilidad.DataValueField = "Id";
+                    ddlDebilidad.DataTextField = "Descripcion";
+                    ddlDebilidad.DataBind();
+                }
+
+                //configuración si estamos modificando.
+                string id = Request.QueryString["id"] != null ? Request.QueryString["id"].ToString() : "";
+                if (id != "" && !IsPostBack)
+                {
+                    PokemonNegocio negocio = new PokemonNegocio();
+                    //List<Pokemon> lista = negocio.listar(id);
+                    //Pokemon seleccionado = lista[0];
+                    Pokemon seleccionado = (negocio.Listar(id))[0];
+
+                    //guardo pokemon seleccionado en session
+                    Session.Add("pokeSeleccionado", seleccionado);
+
+                    //pre cargar todos los campos...
+                    txtId.Text = id;
+                    txtNombre.Text = seleccionado.Nombre;
+                    txtDescripcion.Text = seleccionado.Descripcion;
+                    txtImagenUrl.Text = seleccionado.UrlImagen;
+                    txtNumero.Text = seleccionado.Numero.ToString();
+
+                    ddlTipo.SelectedValue = seleccionado.Tipo.Id.ToString();
+                    ddlDebilidad.SelectedValue = seleccionado.Debilidad.Id.ToString();
+                    txtImagenUrl_TextChanged(sender, e);
+
+                    //configurar acciones
+                    if (!seleccionado.Activo)
+                        btnInactivar.Text = "Reactivar";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Pokemon nuevo = new Pokemon();
+                PokemonNegocio negocio = new PokemonNegocio();
+
+                nuevo.Numero = int.Parse(txtNumero.Text);
+                nuevo.Nombre = txtNombre.Text;
+                nuevo.Descripcion = txtDescripcion.Text;
+                nuevo.UrlImagen = txtImagenUrl.Text;
+
+                nuevo.Tipo = new Elemento();
+                nuevo.Tipo.Id = int.Parse(ddlTipo.SelectedValue);
+                nuevo.Debilidad = new Elemento();
+                nuevo.Debilidad.Id = int.Parse(ddlDebilidad.SelectedValue);
+
+                if (Request.QueryString["id"] != null)
+                {
+                    nuevo.Id = int.Parse(txtId.Text);
+                    negocio.ModificarConSP(nuevo);
+                }
+                else
+                    negocio.AgregarConSP(nuevo);
+
+
+                Response.Redirect("PokemonsLista.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void txtImagenUrl_TextChanged(object sender, EventArgs e)
+        {
+            imgPokemon.ImageUrl = txtImagenUrl.Text;
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ConfirmaEliminacion = true;
+        }
+
+        protected void btnConfirmaEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkConfirmaEliminacion.Checked)
+                {
+                    PokemonNegocio negocio = new PokemonNegocio();
+                    negocio.Eliminar(int.Parse(txtId.Text));
+                    Response.Redirect("PokemonsLista.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void btnInactivar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PokemonNegocio negocio = new PokemonNegocio();
+                Pokemon seleccionado = (Pokemon)Session["pokeSeleccionado"];
+
+                negocio.EliminarLogico(seleccionado.Id, !seleccionado.Activo);
+                Response.Redirect("PokemonsLista.aspx");
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("Error.aspx");
+            }
         }
     }
 }
