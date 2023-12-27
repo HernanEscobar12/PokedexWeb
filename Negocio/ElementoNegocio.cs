42581f809029121de3ac68eb441d15efd529b52f
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:24:20 -0300

    Metodo Listar Desde Db

--- Negocio/ElementoNegocio.cs
+++ Negocio/ElementoNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace Negocio
 {
     public class ElementoNegocio
     {
+        public List<Elemento> Listar()
+        {
+            List<Elemento> lista = new List<Elemento>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("Select Id, Descripcion From ELEMENTOS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Elemento aux = new Elemento();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
     }
 }
