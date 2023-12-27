16235180614e0806ef5bba9f5e4afd3e98f75380
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-27 17:53:36 -0300

    Clase EmailService : Servicio para enviar  Correo al registrar usuario.

---
+++ Dominio/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("ff01e6ec42e120", "78e6c7a7b640fb");
+            server.EnableSsl = true;
+            server.Port = 2525;
+            server.Host = "smtp.mailtrap.io";
+        }
+
+        public void ArmarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("noresponder@pokedexweb.com");
+            email.To.Add(emailDestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            //email.Body = "<h1>Reporte de materias a las que se ha inscripto</h1> <br>Hola, te inscribiste.... bla bla";
+            email.Body = cuerpo;
+
+        }
+
+        public void EnviarEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+
+}
+
