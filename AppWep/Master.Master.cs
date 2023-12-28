75ed4cf6334673eb50646926230b11f6730e8a26
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-28 15:04:13 -0300

    Funcionalidad (Boton Visible  Login  o  Salir )

--- AppWep/Master.Master.cs
+++ AppWep/Master.Master.cs
@@ -1,4 +1,5 @@
-﻿using Negocio;
+﻿using Dominio;
+using Negocio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,10 @@ namespace AppWep
             if(!(Page is Login) && !(Page is Registro) && !(Page is Default))
             if (!(Seguridad.SessionActiva(Session["Trainee"])))
                 Response.Redirect("Login.aspx", false);
+        }
+
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
 
         }
     }
