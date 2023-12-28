b3026a34af396f41e4ef832b6fd2c29391747720
Hernan Escobar <guidohernanescobar@gmail.com>
2023-12-28 00:10:02 -0300

    Clase Seguridad en Capa Negocio con Metodo  para permisos de vistas de page

---
+++ Negocio/Seguridad.cs
@@ -0,0 +1,24 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Negocio
+{
+    public static class Seguridad
+    {
+        public static bool SessionActiva(object user)
+        {
+            Trainee trainee = user != null ? (Trainee) user : null;
+            if (trainee != null && trainee.Id != 0)
+                return true;
+            else 
+                return false;
+
+        }
+
+    }
+}
